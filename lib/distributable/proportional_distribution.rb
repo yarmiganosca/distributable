304c7e1c69943db6811521f18f3df7e7f25c7614
Chris Hoffman <choffman@optoro.com>
2015-08-16 15:53:04 -0400

    Proportional distribution now works.

--- lib/distributable/proportional_distribution.rb
+++ lib/distributable/proportional_distribution.rb
@@ -12,20 +12,34 @@ module Distributable
       @recipients.each_index do |index|
         @recipients[index] *= @multiplier
       end
+
+      @remainder.times do
+        @recipients[index_of_least_disturbance] += 1
+      end
     end
 
     private
+
+    def index_of_least_disturbance
+      disturbances.each.with_index.min_by(&:first).last
+    end
+
+    def disturbances
+      @recipients.map do |recipient|
+        ((recipient + 1) / (recipients_sum + 1).to_f) - (recipient / recipients_sum.to_f)
+      end
+    end
 
     def multiplier
       (@distributable + recipients_sum) / recipients_sum
     end
 
-    def recipients_sum
-      @recipients.reduce(:+)
-    end
-
     def remainder
       (@distributable + recipients_sum) % recipients_sum
+    end
+
+    def recipients_sum
+      @recipients.reduce(:+)
     end
   end
 end
